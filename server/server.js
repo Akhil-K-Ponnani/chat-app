ebdbb496804f6a396cef1a2047f0eff253629642
Akhil K <akhilkponnanivj45@gmail.com>
2022-05-18 21:20:25 +0530

    Updated Backend messages and typing indicator with realtime using socket.io

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Server } from "socket.io";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.js";
 import chatRoutes from "./routes/chat.js"
@@ -21,4 +22,31 @@ app.use(error.errorHandler);
 connectDB.then((data) => console.log(`Database Connected: ${data.connection.host}`)).catch((err) => console.log(err));
 
 /* Listener */
-app.listen(port, () => console.log(`Server started on Port ${port}`));
+const server = app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+/* Socket.io Config */
+const io = new Server(server, { pingTimeout: 60000 })
+io.on("connection", (socket) => {
+    console.log("Connected to Socket.io");
+    socket.on("setup", (user) => {
+        socket.join(user)
+        socket.emit("connected")
+        console.log("User Connected");
+    })
+    socket.on("join-chat", (chat) => {
+        socket.join(chat)
+        console.log(`User joined chat: ${chat}`);
+    })
+    socket.on("send-message", (message) => {
+        message.chat.users.forEach((user) => {
+            if (user._id !== message.sender._id)
+                socket.in(user._id).emit("recieve-message", message)
+        })
+    })
+    socket.on("typing", (chat) => socket.in(chat).emit("typing"))
+    socket.on("stop-typing", (chat) => socket.in(chat).emit("stop-typing"))
+    socket.off("setup", () => {
+        console.log("User Disconnected");
+        socket.leave(user)
+    })
+})
